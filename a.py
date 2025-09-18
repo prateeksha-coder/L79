ace5c32e83d11dc86763dcc9ec69ec8cee75fdd4
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-09-18 15:47:28 +0530

    commit

---
+++ a.py
@@ -0,0 +1,10 @@
+import numpy as np
+a=np.arange(9,dtype=float).reshape(3,3)
+b=np.array([10,10,10])
+print(a)
+print(b)
+print(np.add(a,b))
+print(np.subtract(a,b))
+print(np.multiply(a,b))
+print(np.divide(a,b))
+
